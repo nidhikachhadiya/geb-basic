cea1c644eb6a3d7d80dde6dd972375c47efdc8a2
nidhikachhadiya <nidhi3578@gmail.com>
2019-12-31 17:48:33 +0530

    Add chrome options

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,5 +1,6 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.DesiredCapabilities
 
 waiting {
     timeout = 2
@@ -11,7 +12,16 @@ environments {
 
     // run test in chrome browser
     chrome {
-        driver = { new ChromeDriver() }
+        driver = {
+        ChromeOptions options = new ChromeOptions ()
+            options.addArguments("start-fullscreen")
+            options.addArguments("-incognito")
+            options.setExperimentalOption("useAutomationExtension", false);
+            options.setExperimentalOption("excludeSwitches",Collections.singletonList("enable-automation"));
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+       new ChromeDriver(capabilities)
+        }
     }
 
     //run test with chrome browser in headless mode
